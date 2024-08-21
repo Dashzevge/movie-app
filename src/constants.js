898eca472194dcd0bf563472a8ca7c6596cd9a8a
Dashzevge <76143002+Dashzevge@users.noreply.github.com>
2024-08-21 15:56:33 -0400

    hi
    changes

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ export const API_ENDPOINTS = {
     MOVIES: 'movies',
     GENRES: 'genres/movies',
 };
-
+export const BASE_NAME = process.env.PUBLIC_URL || "/";
 export const BASE_URL = 'https://0kadddxyh3.execute-api.us-east-1.amazonaws.com';
 export const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJvcGVuSnd0NCIsIm5hbWUiOiJPcGVuSldUWzRdIn0.FEyU1vuRnD1wDw2kxuks59N9paTibhKHT8U9cpFw1dM';
 export const NO_IMAGE = 'https://upload.wikimedia.org/wikipedia/commons/d/d1/Image_not_available.png';

39f68318bd2cd35afe040691adfa027a23c74c76
Dashzevge <76143002+Dashzevge@users.noreply.github.com>
2024-08-21 15:41:29 -0400

    Updates

---
+++ precache-manifest.9291cc1ddee8b4c6948f8b42d0d419bc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5b1427b50e493e82afe9b535d316d40",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "a21e0536ba32cecc0211",
+    "url": "/movie-app/static/css/2.b71989ea.chunk.css"
+  },
+  {
+    "revision": "7337b9059001ea8ba41a",
+    "url": "/movie-app/static/css/main.7045b708.chunk.css"
+  },
+  {
+    "revision": "a21e0536ba32cecc0211",
+    "url": "/movie-app/static/js/2.ece621be.chunk.js"
+  },
+  {
+    "revision": "41a2249628309c1cddcd4b9f317ccb73",
+    "url": "/movie-app/static/js/2.ece621be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e161ccdd5ef4dc364abd",
+    "url": "/movie-app/static/js/3.778c1951.chunk.js"
+  },
+  {
+    "revision": "e0cadd4c45e193045149",
+    "url": "/movie-app/static/js/4.b987b510.chunk.js"
+  },
+  {
+    "revision": "fc644ccc226a654bde6e",
+    "url": "/movie-app/static/js/5.360474b6.chunk.js"
+  },
+  {
+    "revision": "7337b9059001ea8ba41a",
+    "url": "/movie-app/static/js/main.957431b9.chunk.js"
+  },
+  {
+    "revision": "e546da3f6c91607860fb",
+    "url": "/movie-app/static/js/runtime-main.0d88ebf4.js"
+  }
+]);
